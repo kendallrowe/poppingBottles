069ffb671e8b951f395d1a76fab2aaded9f6368a
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-30 16:56:38 +0000

    Created first attempt at recursive freebottles function

--- poppinBottles.js
+++ poppinBottles.js
@@ -1,16 +1,21 @@
-// For every two empty bottles, you can get one free (full) bottle of pop
-// For every four bottle caps, you can get one free (full) bottle of pop
-// Each bottle of pop costs $2 to purchase
-
-// Figure out the algorithm that will calculate this. 
-// For example, given a $20 investment, the customer will get 10 bottles of pop. 
-// That gives a supply of both bottles and bottle caps that can be used to redeem for further bottles, 
-// which will then produce a further supply for recycling.
-
-// Test Output
+const freeBottles = function(startingBottles) {
+  // For every two empty bottles, you can get one free (full) bottle of pop
+  let earnedBottles = Math.floor(startingBottles / 2);
+  // For every four bottle caps, you can get one free (full) bottle of pop
+  earnedBottles += Math.floor(startingBottles / 4);
+  return earnedBottles + freeBottles(earnedBottles);
+};
 
 const poppinBottles = function(investment) {
-  return 15;
+  // Each bottle of pop costs $2 to purchase
+  // const changeRemaining = investment % 2;
+  const bottlesBought = Math.floor(investment / 2);
+
+  // Figure out the algorithm that will calculate this. 
+  // For example, given a $20 investment, the customer will get 10 bottles of pop. 
+  // That gives a supply of both bottles and bottle caps that can be used to redeem for further bottles, 
+  // which will then produce a further supply for recycling.
+  return freeBottles(bottlesBought);
 };
 
 module.exports = poppinBottles;
