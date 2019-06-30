67c151c4b5a34ed228f41c7c487a8855c4d1a8b8
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-30 16:27:30 +0000

    Set up test cases and mocha chai in blank files

---
+++ poppinBottles.js
@@ -0,0 +1,12 @@
+// For every two empty bottles, you can get one free (full) bottle of pop
+// For every four bottle caps, you can get one free (full) bottle of pop
+// Each bottle of pop costs $2 to purchase
+
+// Figure out the algorithm that will calculate this. 
+// For example, given a $20 investment, the customer will get 10 bottles of pop. 
+// That gives a supply of both bottles and bottle caps that can be used to redeem for further bottles, 
+// which will then produce a further supply for recycling.
+
+// Test Output
+
+module.exports = poppinBottles;
