ba45ca3b9fe144c9c91fd494c71f1686a5b2414a
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-30 17:51:04 +0000

    Updated function to use objects to be able to increase final reporting output

--- poppinBottles.js
+++ poppinBottles.js
@@ -1,32 +1,37 @@
 const investment = process.argv;
 
 
-const freeBottles = function(emptyBottles, caps) {
+const freeBottles = function(recyclingAndRewards) {
   // For every two empty bottles, you can get one free (full) bottle of pop
-  let earnedBottles = Math.floor(emptyBottles / 2);
-  emptyBottles -= earnedBottles * 2;
+  let earnedBottles = Math.floor(recyclingAndRewards.emptyBottles / 2);
+  recyclingAndRewards.emptyBottles -= earnedBottles * 2;
   // For every four bottle caps, you can get one free (full) bottle of pop
-  earnedBottles += Math.floor(caps / 4);
-  caps -= Math.floor(caps / 4) * 4;
+  earnedBottles += Math.floor(recyclingAndRewards.caps / 4);
+  recyclingAndRewards.caps -= Math.floor(recyclingAndRewards.caps / 4) * 4;
   
-  emptyBottles += earnedBottles;
-  caps += earnedBottles;
+  recyclingAndRewards.emptyBottles += earnedBottles;
+  recyclingAndRewards.caps += earnedBottles;
 
-  if (earnedBottles === 0) {
-    return 0;
-  } else {
-    return earnedBottles + freeBottles(emptyBottles, caps);
+  if (earnedBottles !== 0) {
+    recyclingAndRewards.totalEarnedBottles += earnedBottles
+    recyclingAndRewards = freeBottles(recyclingAndRewards);
   }
+  return recyclingAndRewards
 };
 
 const poppinBottles = function(investment) {
   // Each bottle of pop costs $2 to purchase
   // const changeRemaining = investment % 2;
-  const bottlesBought = Math.floor(investment / 2);
+  let recyclingAndRewards = {};
+  recyclingAndRewards.bottlesBought = Math.floor(investment / 2);
+  recyclingAndRewards.emptyBottles = recyclingAndRewards.bottlesBought;
+  recyclingAndRewards.caps = recyclingAndRewards.bottlesBought;
+  recyclingAndRewards.totalEarnedBottles = 0;
 
-  return bottlesBought + freeBottles(bottlesBought, bottlesBought);
+  recyclingAndRewards = freeBottles(recyclingAndRewards);
+  return recyclingAndRewards.bottlesBought + recyclingAndRewards.totalEarnedBottles;
 };
 
-console.log(poppinBottles(parseInt(investment.slice(2), 10)));
+// console.log(poppinBottles(parseInt(investment.slice(2), 10)));
 
 module.exports = poppinBottles;
